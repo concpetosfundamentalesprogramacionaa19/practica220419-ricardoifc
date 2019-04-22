7c09d2953661617622d60974f08425d00fdc074c
ricardoifc <ricardoifc@me.com>
2019-04-22 16:55:30 -0500

    python2

--- demo2.py
+++ demo2.py
@@ -8,7 +8,6 @@ import sys
 nombre_archivo = sys.argv [0]
 valor1 = sys.argv [1]
 valor2 = sys.argv [2]
-
 print ("variable argv[0]:	%s" % nombre_archivo)
 print ("variable argv[1]:	%s" % valor1)
 print ("variable argv[2]:	%s" % valor2)
